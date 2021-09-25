9c7d11cbae1d2fa8cbbea3f425af52f8b9f67955
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-09-25 21:51:54 +0600

    Solved

--- 21_Reg.js
+++ 21_Reg.js
@@ -3,6 +3,10 @@ You can not use any built-in methods or regex.
 Don't see others comment until you solve it by yourself.
 input: hello!js25@/*bd
 output: hellojs@bd
-input: **/#4Dh - +41aka5@().com8&
-    output: Dhaka@.com
+input: **#4Dh - +41aka5@().com8&
+output: Dhaka@.com
 */
+
+const createReg = str => str.replace(/[^a-z@.?]/gi, '')
+console.log(createReg('**#4Dh - +41aka5@().com8&'))
+console.log(createReg('hello!js25@/*bd'))
