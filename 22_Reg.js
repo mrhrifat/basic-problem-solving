8ba466d6e09f82cbab5458e484eac411acb60284
mrhrifat <mrhrifat383759@gmail.com>
2021-11-26 21:15:32 +0600

    Regex for filter url

--- 22_Reg.js
+++ 22_Reg.js
@@ -8,3 +8,8 @@ Output: javascript
 01) You can't use any built in method
 02) You can't use str.length
 */
+const createReg = str => {
+    return str.replace(/[a-z]+$/g, ' ')
+}
+console.log(createReg('https://www.fb.com/javascript'))
+console.log(createReg('https://www.fb.com/javascript/course/node'))
